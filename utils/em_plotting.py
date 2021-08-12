2190896d3e9366a6f4de0ffb5c7a35763db74247
BRomans <michele.romani.gzl0@gmail.com>
2021-08-12 17:37:06 +0200

    :sparkles: implemented functions to plot average valence-arousal annotations

--- utils/em_plotting.py
+++ utils/em_plotting.py
@@ -61,6 +61,80 @@ def plot_annotations(data):
     plt.show()
 
 
+def plot_avg_annotations(data):
+    plt.rcParams["figure.figsize"] = [15, 7.50]
+    fig, axs = plt.subplots(nrows=1, ncols=2)
+
+    # scatter plot for A trials
+    x = data['trials']['EO/class_1_A']['annotations']['avg_x']
+    y = data['trials']['EO/class_1_A']['annotations']['avg_y']
+    axs[0].scatter(x, y, c='green', marker='^', label='HAHV')
+
+    x = data['trials']['EO/class_2_A']['annotations']['avg_x']
+    y = data['trials']['EO/class_2_A']['annotations']['avg_y']
+    axs[0].scatter(x, y, c='cyan', marker='>', label='LAHV')
+
+    x = data['trials']['EO/class_3_A']['annotations']['avg_x']
+    y = data['trials']['EO/class_3_A']['annotations']['avg_y']
+    axs[0].scatter(x, y, c='blue', marker='v', label='LALV')
+
+    x = data['trials']['EO/class_4_A']['annotations']['avg_x']
+    y = data['trials']['EO/class_4_A']['annotations']['avg_y']
+    axs[0].scatter(x, y, c='orange', marker='<', label='HALV')
+
+    axs[0].legend(loc='upper left')
+    axs[0].set_xlim(-0.5, 0.5)
+    axs[0].set_ylim(-0.5, 0.5)
+    axs[0].set_xlabel('Valence')
+    axs[0].set_ylabel('Arousal')
+    axs[0].set_title("Valence-Arousal annotations for A trials - participant " + data['participant'])
+    axs[0].grid(True)
+
+    # scatter plot for B trials
+    x = data['trials']['EO/class_1_B']['annotations']['avg_x']
+    y = data['trials']['EO/class_1_B']['annotations']['avg_y']
+    axs[1].scatter(x, y, c='green', marker='^', label='HAHV')
+
+    x = data['trials']['EO/class_2_B']['annotations']['avg_x']
+    y = data['trials']['EO/class_2_B']['annotations']['avg_y']
+    axs[1].scatter(x, y, c='cyan', marker='>', label='LAHV')
+
+    x = data['trials']['EO/class_3_B']['annotations']['avg_x']
+    y = data['trials']['EO/class_3_B']['annotations']['avg_y']
+    axs[1].scatter(x, y, c='blue', marker='v', label='LALV')
+
+    x = data['trials']['EO/class_4_B']['annotations']['avg_x']
+    y = data['trials']['EO/class_4_B']['annotations']['avg_y']
+    axs[1].scatter(x, y, c='orange', marker='<', label='HALV')
+
+    axs[1].legend(loc='upper left')
+    axs[1].set_xlim(-0.5, 0.5)
+    axs[1].set_ylim(-0.5, 0.5)
+    axs[1].set_xlabel('Valence')
+    axs[1].set_ylabel('Arousal')
+    axs[1].set_title("Valence-Arousal annotations for B trials - participant " + data['participant'])
+    axs[1].grid(True)
+
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_trial_annotations(x, y):
+    plt.rcParams["figure.figsize"] = [7.50, 7.50]
+    fig, axs = plt.subplots(nrows=1, ncols=1)
+    axs.scatter(x, y, c='red', marker='x', label='Annotations')
+    axs.legend(loc='upper left')
+    axs.set_xlim(-0.5, 0.5)
+    axs.set_ylim(-0.5, 0.5)
+    axs.set_xlabel('Valence')
+    axs.set_ylabel('Arousal')
+    axs.set_title("Valence-Arousal averaged annotations for single trial")
+    axs.grid(True)
+
+    fig.tight_layout()
+    plt.show()
+
+
 def plot_signal(channel_data, duration, sampling_rate=250, raw_xlim=[0, None], raw_ylim=[-0.0001, 0.0001]):
     dt = 1 / sampling_rate  # sampling interval
     Fs = 1 / dt  # sampling frequency of 250Hz
