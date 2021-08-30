7217e7c932333c304a97a8cf6335b02ca4d09301
BRomans <michele.romani.gzl0@gmail.com>
2021-08-30 18:21:53 +0200

    :bug: implemented functions to automate the study of  Valence, Arousal and features correlations

--- classification/em_linear_regression.py
+++ classification/em_linear_regression.py
@@ -1,6 +1,6 @@
+import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
-
 from utils.em_plotting import plot_linear_regression
 
 
@@ -32,3 +32,124 @@ def lin_regression(x_train, y_train, x_test, y_test, d_label='Y', i_label='X', p
 
     return msq, coef, r2
 
+
+def valence_and_neuromarker(prep_dataset, train_participants, test_participants, trial_class, condition, neuromarker):
+    """ Valence annotations and a chosen neuromarker"""
+    x_train = np.array([])
+    y_train = np.array([])
+    x_test = np.array([])
+    y_test = np.array([])
+    for participant_id in train_participants:
+        idx = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'A']['features'][neuromarker]
+        avg_valence = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'A']['features']['avg_x']
+        x_train = np.concatenate((x_train, np.array(avg_valence)))
+        y_train = np.concatenate((y_train, np.array(idx)))
+
+        idx = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']['features'][neuromarker]
+        avg_valence = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'B']['features']['avg_x']
+        x_train = np.concatenate((x_train, np.array(avg_valence)))
+        y_train = np.concatenate((y_train, np.array(idx)))
+
+    for participant_id in test_participants:
+        idx = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'A']['features'][neuromarker]
+        avg_valence = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'A']['features']['avg_x']
+        x_test = np.concatenate((x_test, np.array(avg_valence)))
+        y_test = np.concatenate((y_test, np.array(idx)))
+
+        idx = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']['features'][neuromarker]
+        avg_valence = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'B']['features']['avg_x']
+        x_test = np.concatenate((x_test, np.array(avg_valence)))
+        y_test = np.concatenate((y_test, np.array(idx)))
+    return x_train, y_train, x_test, y_test
+
+
+def arousal_and_neuromarker(prep_dataset, train_participants, test_participants, trial_class, condition, neuromarker):
+    """ Arousal annotations and a chosen neuromarker"""
+    x_train = np.array([])
+    y_train = np.array([])
+    x_test = np.array([])
+    y_test = np.array([])
+    for participant_id in train_participants:
+        idx = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'A']['features'][neuromarker]
+        avg_arousal = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'A']['features']['avg_y']
+        x_train = np.concatenate((x_train, np.array(avg_arousal)))
+        y_train = np.concatenate((y_train, np.array(idx)))
+
+        idx = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']['features'][neuromarker]
+        avg_arousal = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'B']['features']['avg_y']
+        x_train = np.concatenate((x_train, np.array(avg_arousal)))
+        y_train = np.concatenate((y_train, np.array(idx)))
+
+    for participant_id in test_participants:
+        idx = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'A']['features'][neuromarker]
+        avg_arousal = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'A']['features']['avg_y']
+        x_test = np.concatenate((x_test, np.array(avg_arousal)))
+        y_test = np.concatenate((y_test, np.array(idx)))
+
+        idx = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']['features'][neuromarker]
+        avg_arousal = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'B']['features']['avg_y']
+        x_test = np.concatenate((x_test, np.array(avg_arousal)))
+        y_test = np.concatenate((y_test, np.array(idx)))
+    return x_train, y_train, x_test, y_test
+
+
+# NOT working, to adjust for automation
+def prepare_regression_data_annotations(prep_dataset,train_participants, test_participants, independent, dependent, condition,
+                                        trial_class):
+    x_train = np.array([])
+    y_train = np.array([])
+    x_test = np.array([])
+    y_test = np.array([])
+    for participant_id in train_participants:
+        i = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'A']['features'][independent]
+        d = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'A']['features'][dependent]
+        x_train = np.concatenate((x_train, np.array(i)))
+        y_train = np.concatenate((y_train, np.array(d)))
+
+        i = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']['features'][independent]
+        d = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'B']['features'][dependent]
+        x_train = np.concatenate((x_train, np.array(i)))
+        y_train = np.concatenate((y_train, np.array(d)))
+
+    for participant_id in test_participants:
+        i = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'A']['features'][independent]
+        d = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'A']['features'][dependent]
+        x_test = np.concatenate((x_test, np.array(i)))
+        y_test = np.concatenate((y_test, np.array(d)))
+
+        i = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']['features'][independent]
+        d = prep_dataset[participant_id]['trials']['EO' + '/' + trial_class + 'B']['features'][dependent]
+        x_test = np.concatenate((x_test, np.array(i)))
+        y_test = np.concatenate((y_test, np.array(d)))
+
+    return x_train, y_train, x_test, y_test
+
+
+def liking_familiarity_correlation(prep_dataset, train_participants, test_participants, condition, trial_class, dependent, independent):
+    """ Correlations between liking and familiarity """
+    x_train = np.array([])
+    y_train = np.array([])
+    x_test = np.array([])
+    y_test = np.array([])
+    for participant_id in train_participants:
+        i = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'A']['features'][independent]
+        d = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'A']['features'][dependent]
+        x_train = np.append(x_train, int(i))
+        y_train = np.append(y_train, int(d))
+
+        i = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']['features'][independent]
+        d = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']['features'][dependent]
+        x_train = np.append(x_train, int(i))
+        y_train = np.append(y_train, int(d))
+
+    for participant_id in test_participants:
+        i = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'A']['features'][independent]
+        d = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'A']['features'][dependent]
+        x_test = np.append(x_test, int(i))
+        y_test = np.append(y_test, int(d))
+
+        i = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']['features'][independent]
+        d = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']['features'][dependent]
+        x_test = np.append(x_test, int(i))
+        y_test = np.append(y_test, int(d))
+    return x_train, y_train, x_test, y_test
