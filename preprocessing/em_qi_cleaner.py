31b49f8249a27b3ab4b11a7e27c2b1ae1f26c16e
BRomans <michele.romani.gzl0@gmail.com>
2021-09-08 16:24:56 +0200

    :sparkles: cleaning functions handle eeg as normal array

--- preprocessing/em_qi_cleaner.py
+++ preprocessing/em_qi_cleaner.py
@@ -1,3 +1,5 @@
+import copy
+
 import numpy as np
 from mbt_pyspt.models.mybraineegdata import MyBrainEEGData
 
@@ -27,8 +29,8 @@ def qi_data_removal(participant, trial_duration=60, qi_window_size=6, qi_thresho
 
             # Now loop through the time windows and save the preprocessed EEG in a new variable withouht the low quality data by copying window by window (1 * sampling_rate)
             cleaned_eeg = [[], []]
-            split_eeg_F4 = np.array_split(prep_eeg.matrix_data[0], n_windows)
-            split_eeg_F3 = np.array_split(prep_eeg.matrix_data[1], n_windows)
+            split_eeg_F4 = np.array_split(prep_eeg[0], n_windows)
+            split_eeg_F3 = np.array_split(prep_eeg[1], n_windows)
             annotations['c_x'] = []
             annotations['c_y'] = []
             ann_sr = int(round(len(annotations["x"]) / trial_duration))
@@ -37,8 +39,8 @@ def qi_data_removal(participant, trial_duration=60, qi_window_size=6, qi_thresho
 
             for idx in range(0, n_windows):
                 if win_to_remove[idx] == 0:
-                    cleaned_eeg[0].extend(split_eeg_F4[idx])
-                    cleaned_eeg[1].extend(split_eeg_F3[idx])
+                    cleaned_eeg[0].extend(copy.deepcopy(split_eeg_F4[idx]))
+                    cleaned_eeg[1].extend(copy.deepcopy(split_eeg_F3[idx]))
                     annotations['c_x'].extend(split_annotations_x[idx])
                     annotations['c_y'].extend(split_annotations_y[idx])
             cleaned_eeg = np.array(cleaned_eeg)
@@ -46,7 +48,7 @@ def qi_data_removal(participant, trial_duration=60, qi_window_size=6, qi_thresho
             print("Clean seconds of annotations:", len(annotations['c_x']) / ann_sr,
                   len(annotations['c_y']) / ann_sr)
 
-            participant['trials'][trial]['clean_eeg'] = MyBrainEEGData(cleaned_eeg, sr, channel_locations)
+            participant['trials'][trial]['clean_eeg'] = cleaned_eeg
             participant['trials'][trial]['c_windows'] = n_windows - np.count_nonzero(win_to_remove)
             participant['trials'][trial]['all_windows'] = win_to_remove
             perc_bad = round(np.count_nonzero(win_to_remove) / len(win_to_remove) * 100, 2)
@@ -83,75 +85,3 @@ def windows_to_remove(trial_duration, qi_threshold, qualities, qi_window_size):
     return win_to_remove
 
 
-def qi_data_removal_old(participant, trial_duration=60, qi_threshold=1.0, allowed_loss=50, sr=250):
-    """ Old version of the data removal based on Quality Checker, do not use """
-    trials = participant['trials']
-    channel_locations = participant['acquisitionLocation']
-    participant['bad_quality'] = False
-    for trial in trials:
-        if trial.startswith('EO') or trial.startswith('EC'):
-            print(trial)
-            # find time windows that should be removed according to QI
-
-            prep_eeg = participant['trials'][trial]['prep_eeg']
-            qualities = participant['trials'][trial]['qualities']
-            annotations = participant['trials'][trial]['annotations']
-            win_to_remove = np.zeros(trial_duration, dtype=int)
-
-            # save the indexes that should be removed. Both channels are iterated and if one of them is below the desired quality, remove both
-            iterator = 0
-            for value in qualities[0]:
-                if float(value) < qi_threshold:
-                    win_to_remove[iterator] = 1
-                iterator += 1
-
-            iterator = 0
-            for value in qualities[1]:
-                if float(value) < qi_threshold:
-                    win_to_remove[iterator] = 1
-                iterator += 1
-            print(win_to_remove)
-            perc_bad = round(np.count_nonzero(win_to_remove) / len(win_to_remove) * 100, 2)
-            print("Percentage of data to be pruned: " + str(perc_bad) + "%")
-
-            if perc_bad > allowed_loss:
-                participant['bad_quality'] = True
-
-            # Now loop through the time windows and save the preprocessed EEG in a new variable withouht the low quality data by copying window by window (1 * sampling_rate)
-            cleaned_eeg = [[], []]
-            split_eeg_F4 = np.array_split(prep_eeg.matrix_data[0], trial_duration)
-            split_eeg_F3 = np.array_split(prep_eeg.matrix_data[1], trial_duration)
-            idx = 0
-            cleaned_duration = 0
-            for window in win_to_remove:
-                if int(window) == 0:
-                    cleaned_eeg[0].extend(split_eeg_F4[idx])
-                    cleaned_eeg[1].extend(split_eeg_F3[idx])
-                    cleaned_duration += 1
-                idx += 1
-            cleaned_eeg = np.array(cleaned_eeg)
-            print("Clean seconds of EEG:", cleaned_eeg.shape[1] / 250)
-            participant['trials'][trial]['clean_eeg'] = MyBrainEEGData(cleaned_eeg, sr, channel_locations)
-            participant['trials'][trial]['c_duration'] = cleaned_duration
-
-            # Finally, we also remove the annotations associated with each time window for EO trials
-            if trial.startswith('EO'):
-                x = []
-                y = []
-                annotations['c_x'] = []
-                annotations['c_y'] = []
-                ann_sr = int(round(len(annotations["x"]) / trial_duration))
-                split_annotations_x = np.array_split(annotations["x"], trial_duration)
-                split_annotations_y = np.array_split(annotations["y"], trial_duration)
-                print("Annotation sampling rate", ann_sr)
-                # print(len(annotations['x']), len(annotations['y']))
-                idx = 0
-                for window in win_to_remove:
-                    if int(window) == 0:
-                        x.extend(split_annotations_x[idx])
-                        y.extend(split_annotations_y[idx])
-                    idx += 1
-                annotations['c_x'] = x
-                annotations['c_y'] = y
-                print("Clean seconds of annotations:", len(annotations['c_x']) / ann_sr,
-                      len(annotations['c_y']) / ann_sr)
