51e99b8236912f96685a687e8b327a2183b6f365
BRomans <michele.romani.gzl0@gmail.com>
2021-10-04 09:58:53 +0200

    :sparkles: new functions to compute baseline normalized features

--- preprocessing/em_feature_extraction.py
+++ preprocessing/em_feature_extraction.py
@@ -3,6 +3,118 @@ from mbt_pyspt.models.mybraineegdata import MyBrainEEGData
 from mbt_pyspt.modules.featuresextractionflow import FeaturesExtractionFlow
 
 
+def compute_participant_features_baseline_normalized(participant, split_data, sr, loc, cleaned_eeg=False, skip_qc=True):
+    """ Retrieve the alpha, beta and theta power in specified time windows to calculate the neuromarkers
+    Normalization: should I normalize? while extracting frequency bands or after computing the neuromarkers?
+
+    Alpha Power:
+    increasing/decreasing frontal alpha power can correlate with increasing/decreasing arousal
+    https://www.tandfonline.com/doi/abs/10.1080/02699930126048
+
+    SASI index:
+    (beta - theta)/(beta + theta). Increases for Negative emotions and decreases for
+    positive emotions
+    https://pubmed.ncbi.nlm.nih.gov/26738175/
+    https://ieeexplore.ieee.org/document/8217815
+
+    AW Index:
+    alphaAF4 - alphaAF3. Left hemisphere (AF3) for Positive Valence, right hemisphere (AF4) for Negative Valence.
+    If AWIdx is positive, there is a right tendency and NV. if AWIdx is negative, there is left tendency and PV.
+    What if values are negatives? Should we subtract absolute values?
+    https://www.tandfonline.com/doi/abs/10.1080/02699930126048
+
+    FMT Index:
+    mean Theta power stimulus/ mean Theta power baseline for Fz channel. It follows approach-withdrawal
+    tendencies similarly to AWIndex but it increases/decreases in correlation with pleasant/unpleasant music.
+    Should we investigate correlation with liking?
+    https://stefan-koelsch.de/papers/Sammler_2007_music-emotion-Fm-theta-HR-EEG.pdf
+
+    FFT-based or wavelet based features:
+    Nothing for the moment
+
+    """
+
+    theta_extraction = [("get_power_theta", {'samp_rate': 250, 'l_freq': 4.0, 'h_freq': 8.0})]
+    alpha_extraction = [("get_power_alpha", {'samp_rate': 250, 'l_freq': 8.0, 'h_freq': 13.0})]
+    beta_extraction = [("get_power_beta", {'samp_rate': 250, 'l_freq': 13.0, 'h_freq': 28.0})]
+
+    baseline_eeg = MyBrainEEGData(participant['baseline_eeg'], sr, loc)
+
+    extraction = FeaturesExtractionFlow(baseline_eeg, features_list=theta_extraction)
+    theta_bas, theta_bas_labels = extraction()
+    print("Baseline Theta Power", theta_bas, theta_bas_labels)
+
+    extraction = FeaturesExtractionFlow(baseline_eeg, features_list=alpha_extraction)
+    alpha_bas, alpha_bas_labels = extraction()
+    print("Baseline Alpha Power", alpha_bas, alpha_bas_labels)
+
+    extraction = FeaturesExtractionFlow(baseline_eeg, features_list=beta_extraction)
+    beta_bas, beta_bas_labels = extraction()
+    print("Baseline Beta Power", beta_bas, beta_bas_labels)
+
+    trials = participant['trials']
+    eeg_label = 'prep_eeg'
+    if cleaned_eeg:
+        eeg_label = 'clean_eeg'
+    for trial in trials:
+        if trial.startswith('EO') or trial.startswith('EC'):
+            n_win = trials[trial]['c_windows']
+            if not trials[trial]['bad_quality'] or skip_qc:
+                eeg = MyBrainEEGData(trials[trial][eeg_label], sr, loc)
+
+                extraction = FeaturesExtractionFlow(eeg, features_list=theta_extraction, split_data=split_data)
+                theta_powers, labels = extraction()
+                norm_theta_pow = percent_normalization(theta_powers, theta_bas)
+                print("Theta  Powers", theta_powers)
+                print("Norm Theta  Powers", norm_theta_pow)
+
+                extraction = FeaturesExtractionFlow(eeg, features_list=alpha_extraction, split_data=split_data)
+                alpha_powers, labels = extraction()
+                print("Alpha  Powers", alpha_powers, labels)
+
+                extraction = FeaturesExtractionFlow(eeg, features_list=beta_extraction, split_data=split_data)
+                beta_powers, labels = extraction()
+                print("Beta  Powers", beta_powers, labels)
+
+                aw_idx = np.subtract(alpha_powers[0], alpha_powers[1])  # alpha AF4 - alpha AF3
+                print("AWI", aw_idx)
+
+                fmt_idx = np.mean(theta_powers, axis=0)  # mean(theta AF4, theta AF3) element-wise
+                print("FMT", fmt_idx)
+
+                sasi_idx = np.array([
+                    np.divide(np.subtract(beta_powers[0], theta_powers[0]), np.add(beta_powers[0], theta_powers[0])),
+                    np.divide(np.subtract(beta_powers[1], theta_powers[1]), np.add(beta_powers[1], theta_powers[1]))
+                ])  # (beta - theta / beta + theta)AF4, (beta - theta / beta + theta)AF3
+                print("SASI", sasi_idx)
+
+                if "features" not in trials[trial]:
+                    trials[trial]['features'] = dict()
+                trials[trial]['features']['alpha_pow'] = alpha_powers
+                trials[trial]['features']['aw_idx'] = aw_idx
+                trials[trial]['features']['sasi_idx'] = sasi_idx
+                trials[trial]['features']['fmt_idx'] = fmt_idx
+                trials[trial]['features']['familiarity'] = trials[trial]['annotations']['familiarity']
+                trials[trial]['features']['liking'] = trials[trial]['annotations']['liking']
+
+
+def percent_normalization(freq_power, bas_freq_power):
+    normalized_channels = []
+    for i in range(0, freq_power.shape[0]):
+        norm_channel_pow = 100 * ((freq_power[i] - bas_freq_power[i]) / bas_freq_power[i])
+        normalized_channels.append(norm_channel_pow)
+    return np.array(normalized_channels)
+
+
+def decibel_normalization(freq_power, bas_freq_power):
+    normalized_channels = []
+    for i in range(0, freq_power.shape[0]):
+        norm_channel_pow = 10 * np.log10(freq_power[i] / bas_freq_power[i])
+        normalized_channels.append(norm_channel_pow)
+    return np.array(normalized_channels)
+
+
+@DeprecationWarning
 def compute_participant_features(data, ff_list, split_data, sr, loc, cleaned_eeg=False, skip_qc=True):
     """ Retrieve the alpha, beta and theta power in specified time windows to calculate the neuromarkers
     Normalization: should I normalize? while extracting frequency bands or after computing the neuromarkers?
@@ -74,7 +186,3 @@ def compute_participant_features(data, ff_list, split_data, sr, loc, cleaned_eeg
                 trials[trial]['features']['fmt_idx'] = fmt_idx
                 trials[trial]['features']['familiarity'] = trials[trial]['annotations']['familiarity']
                 trials[trial]['features']['liking'] = trials[trial]['annotations']['liking']
-
-
-
-
