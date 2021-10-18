89320ee4e956bcc2d0bd55ed131ff57970a56046
BRomans <michele.romani.gzl0@gmail.com>
2021-10-18 12:20:02 +0200

    :sparkles: Updated labels extraction to add option for numeric labels

--- classification/em_features_aggregator.py
+++ classification/em_features_aggregator.py
@@ -20,31 +20,38 @@ def get_arousal_annotations(prep_dataset, participants_subset, condition, skip_q
     return arousal_ann
 
 
-def get_arousal_labels(prep_dataset, participants_subset, condition, skip_qc=True):
+def get_arousal_labels(prep_dataset, participants_subset, condition, skip_qc=True, string_labels=True):
     """ All arousal labels filtered by condition"""
     arousal_labels = np.array([])
     t_classes = ['class_1_', 'class_2_', 'class_3_', 'class_4_']
+    pos_label = "HA" if string_labels else 1
+    neg_label = "LA" if string_labels else -1
+
     for participant_id in participants_subset:
         trials = prep_dataset[participant_id]['trials']
         for trial_class in t_classes:
             trial = trials[condition + '/' + trial_class + 'A']
             if not trial['bad_quality'] or skip_qc:
                 avg_arousal = trial['features']['avg_y']
-                labels = ["HA" if i > 0 else "LA" for i in avg_arousal]
+                labels = [pos_label if i > 0 else neg_label for i in avg_arousal]
                 arousal_labels = np.concatenate((arousal_labels, np.array(labels)))
 
             trial = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']
             if not trial['bad_quality'] or skip_qc:
                 avg_arousal = trial['features']['avg_y']
-                labels = ["HA" if i > 0 else "LA" for i in avg_arousal]
+                labels = [pos_label if i > 0 else neg_label for i in avg_arousal]
                 arousal_labels = np.concatenate((arousal_labels, np.array(labels)))
     return arousal_labels
 
 
-def get_arousal_labels_with_neutral(prep_dataset, participants_subset, condition, skip_qc=True):
+def get_arousal_labels_with_neutral(prep_dataset, participants_subset, condition, skip_qc=True, string_labels=True):
     """ All arousal labels filtered by condition and converted into three classes: HA, LA and Neutral"""
     arousal_labels = np.array([])
     t_classes = ['class_1_', 'class_2_', 'class_3_', 'class_4_']
+    pos_label = "HA" if string_labels else 1
+    neg_label = "LA" if string_labels else -1
+    neu_label = "N" if string_labels else 0
+
     for participant_id in participants_subset:
         trials = prep_dataset[participant_id]['trials']
         for trial_class in t_classes:
@@ -54,11 +61,11 @@ def get_arousal_labels_with_neutral(prep_dataset, participants_subset, condition
                 labels = []
                 for i in avg_arousal:
                     if i > 0.1:
-                        labels.append("HA")
+                        labels.append(pos_label)
                     elif i < -0.1:
-                        labels.append("LA")
+                        labels.append(neg_label)
                     else:
-                        labels.append("N")
+                        labels.append(neu_label)
                 arousal_labels = np.concatenate((arousal_labels, np.array(labels)))
 
             trial = prep_dataset[participant_id]['trials'][condition + '/' + trial_class + 'B']
@@ -67,11 +74,11 @@ def get_arousal_labels_with_neutral(prep_dataset, participants_subset, condition
                 labels = []
                 for i in avg_arousal:
                     if i > 0.1:
-                        labels.append("HA")
+                        labels.append(pos_label)
                     elif i < -0.1:
-                        labels.append("LA")
+                        labels.append(neg_label)
                     else:
-                        labels.append("N")
+                        labels.append(neu_label)
                 arousal_labels = np.concatenate((arousal_labels, np.array(labels)))
     return arousal_labels
 
@@ -95,31 +102,38 @@ def get_valence_annotations(prep_dataset, participants_subset, condition, skip_q
     return valence_ann
 
 
-def get_valence_labels(prep_dataset, participants_subset, condition, skip_qc=True):
+def get_valence_labels(prep_dataset, participants_subset, condition, skip_qc=True, string_labels=True):
     """ All valence labels filtered by condition"""
     valence_labels = np.array([])
     t_classes = ['class_1_', 'class_2_', 'class_3_', 'class_4_']
+    pos_label = "HV" if string_labels else 1
+    neg_label = "LV" if string_labels else -1
+
     for participant_id in participants_subset:
         trials = prep_dataset[participant_id]['trials']
         for trial_class in t_classes:
             trial = trials[condition + '/' + trial_class + 'A']
             if not trial['bad_quality'] or skip_qc:
                 avg_valence = trial['features']['avg_x']
-                labels = ["HV" if i > 0 else "LV" for i in avg_valence]
+                labels = [pos_label if i > 0 else neg_label for i in avg_valence]
                 valence_labels = np.concatenate((valence_labels, np.array(labels)))
 
             trial = trials[condition + '/' + trial_class + 'B']
             if not trial['bad_quality'] or skip_qc:
                 avg_valence = trial['features']['avg_x']
-                labels = ["HV" if i > 0 else "LV" for i in avg_valence]
+                labels = [pos_label if i > 0 else neg_label for i in avg_valence]
                 valence_labels = np.concatenate((valence_labels, np.array(labels)))
     return valence_labels
 
 
-def get_valence_labels_with_neutral(prep_dataset, participants_subset, condition, skip_qc=True):
+def get_valence_labels_with_neutral(prep_dataset, participants_subset, condition, skip_qc=True, string_labels=True):
     """ All valence labels filtered by condition converted into three classes: HV, LV and Neutral"""
     valence_labels = np.array([])
     t_classes = ['class_1_', 'class_2_', 'class_3_', 'class_4_']
+    pos_label = "HV" if string_labels else 1
+    neg_label = "LV" if string_labels else -1
+    neu_label = "N" if string_labels else 0
+
     for participant_id in participants_subset:
         trials = prep_dataset[participant_id]['trials']
         for trial_class in t_classes:
@@ -129,9 +143,9 @@ def get_valence_labels_with_neutral(prep_dataset, participants_subset, condition
                 labels = []
                 for i in avg_valence:
                     if i > 0.1:
-                        labels.append("HV")
+                        labels.append(pos_label)
                     elif i < -0.1:
-                        labels.append("LV")
+                        labels.append(neg_label)
                     else:
                         labels.append("N")
                 valence_labels = np.concatenate((valence_labels, np.array(labels)))
@@ -142,20 +156,24 @@ def get_valence_labels_with_neutral(prep_dataset, participants_subset, condition
                 labels = []
                 for i in avg_valence:
                     if i > 0.1:
-                        labels.append("HV")
+                        labels.append(pos_label)
                     elif i < -0.1:
-                        labels.append("LV")
+                        labels.append(neg_label)
                     else:
-                        labels.append("N")
+                        labels.append(neu_label)
                 valence_labels = np.concatenate((valence_labels, np.array(labels)))
     return valence_labels
 
 
-
-def get_valence_arousal_labels(prep_dataset, participants_subset, condition, skip_qc=True):
+def get_valence_arousal_labels(prep_dataset, participants_subset, condition, skip_qc=True, string_labels=True):
     """ All VA labels filtered by condition"""
     va_labels = np.array([])
     t_classes = ['class_1_', 'class_2_', 'class_3_', 'class_4_']
+    hahv_label = "HAHV" if string_labels else 1
+    lahv_label = "LAHV" if string_labels else 2
+    lalv_label = "LALV" if string_labels else 3
+    halv_label = "HALV" if string_labels else 4
+
     for participant_id in participants_subset:
         trials = prep_dataset[participant_id]['trials']
         for trial_class in t_classes:
@@ -166,13 +184,13 @@ def get_valence_arousal_labels(prep_dataset, participants_subset, condition, ski
                 labels = []
                 for i in range(0, len(avg_arousal)):
                     if avg_arousal[i] > 0 and avg_valence[i] > 0:
-                        labels.append('HAHV')
+                        labels.append(hahv_label)
                     elif avg_arousal[i] <= 0 and avg_valence[i] > 0:
-                        labels.append('LAHV')
+                        labels.append(lahv_label)
                     elif avg_arousal[i] <= 0 and avg_valence[i] <= 0:
-                        labels.append('LALV')
+                        labels.append(lalv_label)
                     elif avg_arousal[i] > 0 and avg_valence[i] <= 0:
-                        labels.append('HALV')
+                        labels.append(halv_label)
                 va_labels = np.concatenate((va_labels, np.array(labels)))
 
             trial = trials[condition + '/' + trial_class + 'B']
@@ -182,13 +200,13 @@ def get_valence_arousal_labels(prep_dataset, participants_subset, condition, ski
                 labels = []
                 for i in range(0, len(avg_arousal)):
                     if avg_arousal[i] > 0 and avg_valence[i] > 0:
-                        labels.append('HAHV')
+                        labels.append(hahv_label)
                     elif avg_arousal[i] <= 0 and avg_valence[i] > 0:
-                        labels.append('LAHV')
+                        labels.append(lahv_label)
                     elif avg_arousal[i] <= 0 and avg_valence[i] <= 0:
-                        labels.append('LALV')
+                        labels.append(lalv_label)
                     elif avg_arousal[i] > 0 and avg_valence[i] <= 0:
-                        labels.append('HALV')
+                        labels.append(halv_label)
                 va_labels = np.concatenate((va_labels, np.array(labels)))
     return va_labels
 
